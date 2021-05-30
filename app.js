73a1d43d7a459cad4b3499fedb65072e7e8bebd4
Shivam Chamoli <shivamchamoli99@gmail.com>
2021-05-30 19:15:44 +0530

    Seccond Commit
    Added work page

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
+//Requiring node modules
 const express= require("express");
-const nodyparser= require("body-parser");
+const bodyparser= require("body-parser");
 
+// Code to run the modules
 const app= express();
-
 app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
 
+//Global Variables
+var toDo= ["Learn Node", "Learn React", "Read Moby Dick"];
+let workToDo=[];
+//Get Requests
 app.get("/", function(req,res){
     var today= new Date();
-    var day="";
-    switch(today.getDay()){
-        case 1:
-            day="Monday";
-            break;
-        case 2:
-            day="Tuesday";
-            break;
-        case 3:
-            day="Wednesday";
-            break;
-        case 4:
-            day="Thursday";
-            break;
-        case 5:
-            day="Friday";
-            break;
-        case 6:
-            day="Saturday";
-            break;
-        case 0:
-            day= "Sunday";    
-            break;
-    }
+    const options={
+        weekday : "long",
+        day: "numeric" ,
+        month: "long",
+        year: "numeric"
+    };
 
-    res.render("index", {kindOfDay: day});
+    var day=today.toLocaleDateString("en-US",options);
+    
+    res.render("index", {listType: day, toDo : toDo});
 
+});
+
+app.get("/work", function(req,res){      
+    res.render("index", {listType: "Work", toDo : workToDo});
 })
 
+//Post Requests
+app.post("/", function(req,res){
+    console.log(req.body);
+    const todoItem= req.body.item;
+    if(req.body.list== "Work"){
+        workToDo.push(todoItem);
+        res.redirect("/work");
+    }else{
+        toDo.push(todoItem);
+        res.redirect("/");
+    }
+    
+});
+
+//Listen Request
 app.listen("3000",function(){
     console.log("Server Started at port 3000");
 })
